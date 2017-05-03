f468adb02cf30d1878806bde7e230bbbc8fda37a
Honza Rychnovský <honzar@appsdevteam.com>
2017-05-03 17:58:23 +0200

    EqualsUtils done + javadoc

--- library/src/main/java/com/honzar/adtutils/library/EqualsUtils.java
+++ library/src/main/java/com/honzar/adtutils/library/EqualsUtils.java
@@ -2,23 +2,48 @@ package com.honzar.adtutils.library;
 
 import android.location.Location;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class EqualsUtils {
+
 	public static int HASHCODE_INVALID = 654123456;
 	public static int HASHCODE_INVALID_REPLACEMENT = 654123457;
-	
-    public static <T> int hashCodeCheckNull(T o) {
-        return o == null ? 0 : o.hashCode();
+
+    /**
+     * Returns objects hashcode
+     *
+     * @param object
+     *
+     * @return objects hashcode
+     */
+    public static <T> int hashCodeCheckNull(T object)
+    {
+        return object == null ? 0 : object.hashCode();
     }
-    
-    public static <T> boolean equalsCheckNull(T lhs, T rhs) {
+
+    /**
+     * Compare two objects for equality
+     *
+     * @param lhs
+     * @param rhs
+     *
+     * @return true in case of objects equality, false otherwise
+     */
+    public static <T> boolean equalsCheckNull(T lhs, T rhs)
+    {
         return lhs == null ? rhs == null : lhs.equals(rhs);
     }
-    
-    public static int itemsHashCode(List<?> items) {
+
+    /**
+     * Returns hashcode of list of items
+     *
+     * @param items
+     *
+     * @return items hashcode
+     */
+    public static int itemsHashCode(List<?> items)
+    {
         int result = 17; 
         if (items != null) {
             for (int i = 0; i < items.size(); i++) {
@@ -28,7 +53,15 @@ public class EqualsUtils {
         return result;
     }
 
-    public static int itemsHashCode(int[] items) {
+    /**
+     * Returns hashcode of array of integers
+     *
+     * @param items
+     *
+     * @return integers hashcode
+     */
+    public static int itemsHashCode(int[] items)
+    {
         int result = 17;
         if (items != null) {
             for (int i = 0; i < items.length; i++) {
@@ -38,8 +71,16 @@ public class EqualsUtils {
         return result;
     }
 
-    
-    public static boolean itemsEqual(List<?> lhs, List<?> rhs) {
+    /**
+     * Compares two lists for equality
+     *
+     * @param lhs
+     * @param rhs
+     *
+     * @return true in case of lists equality, false otherwise
+     */
+    public static boolean itemsEqual(List<?> lhs, List<?> rhs)
+    {
         if (lhs == rhs)
             return true;
         if (lhs == null || rhs == null)
@@ -59,6 +100,14 @@ public class EqualsUtils {
         return true;
     }
 
+    /**
+     * Compares two arrays of integers for equality
+     *
+     * @param lhs
+     * @param rhs
+     *
+     * @return true in case of arrays equality, false otherwise
+     */
     public static boolean itemsEqual(int[] lhs, int[] rhs) {
         if (lhs == rhs)
             return true;
@@ -71,126 +120,126 @@ public class EqualsUtils {
         }
         return true;
     }
-    
-    public static int itemsOfItemsHashCode(List<? extends List<?>> items) {
-        int result = 17; 
-        if (items != null) {
-            for (int i = 0; i < items.size(); i++) {
-                result = 31 * result + itemsHashCode(items.get(i));
-            }
-        }
-        return result;
-    }
-    
-    public static boolean itemsOfItemsEqual(List<? extends List<?>> lhs, List<? extends List<?>> rhs) {
-        if (lhs == rhs)
-            return true;
-        if (lhs == null || rhs == null)
-            return false;           
-        if (lhs.size() != rhs.size())
-            return false;
-                
-        for (int i = 0; i < lhs.size(); i++) {
-            if (!itemsEqual(lhs.get(i), rhs.get(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    public static boolean stringEquality(String s1, String s2) {
-        if (s1 == null && s2 == null) return true;
-        if (s1 == null) return false;
-        if (s2 == null) return false;
+    /**
+     * Compares two strings for equality
+     *
+     * @param s1
+     * @param s2
+     *
+     * @return true in case of strings equality, false otherwise
+     */
+    public static boolean stringEquality(String s1, String s2)
+    {
+        if (s1 == null && s2 == null)
+            return true;
+        if (s1 == null)
+            return false;
+        if (s2 == null)
+            return false;
 
         return s1.equals(s2);
     }
 
-    public static boolean listEquals(ArrayList<?> first, ArrayList<?> second)
+    /**
+     * Compares two integers for equality
+     *
+     * @param i1
+     * @param i2
+     *
+     * @return true in case of integers equality, false otherwise
+     */
+    public static boolean integerEquality(Integer i1, Integer i2)
     {
-        if (first == second) return true;
-        if (first == null) return false;
-        if (second == null) return false;
-
-        if (first.size() != second.size()) {
+        if (i1 == null && i2 == null)
+            return true;
+        if (i1 == null)
             return false;
-        }
-        for (int i = 0; i < first.size(); i++) {
-            if (!first.get(i).equals(second.get(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean integerEquality(Integer i1, Integer i2) {
-        if (i1 == null && i2 == null) return true;
-        if (i1 == null) return false;
-        if (i2 == null) return false;
+        if (i2 == null)
+            return false;
 
         return i1.intValue() == i2.intValue();
     }
 
-    public static boolean longEquality(Long l1, Long l2) {
-        if (l1 == null && l2 == null) return true;
-        if (l1 == null) return false;
-        if (l2 == null) return false;
+    /**
+     * Compares two longs for equality
+     *
+     * @param l1
+     * @param l2
+     *
+     * @return true in case of longs equality, false otherwise
+     */
+    public static boolean longEquality(Long l1, Long l2)
+    {
+        if (l1 == null && l2 == null)
+            return true;
+        if (l1 == null)
+            return false;
+        if (l2 == null)
+            return false;
 
         return l1.longValue() == l2.longValue();
     }
 
-    public static boolean integesrEquality(Integer[] is1, Integer[] is2) {
-        if (is1 == null && is2 == null) return true;
-        if (is1 == null) return false;
-        if (is2 == null) return false;
+    /**
+     * Compares two floats for equality
+     *
+     * @param f1
+     * @param f2
+     *
+     * @return true in case of longs equality, false otherwise
+     */
+    public static boolean floatEquality(Float f1, Float f2)
+    {
+        if (f1 == null && f2 == null)
+            return true;
+        if (f1 == null)
+            return false;
+        if (f2 == null)
+            return false;
+
+        return f1.floatValue() == f2.floatValue();
+    }
+
+    /**
+     * Compares two lists of integers for equality
+     *
+     * @param is1
+     * @param is2
+     *
+     * @return true in case of arrays equality, false otherwise
+     */
+    public static boolean integesrEquality(Integer[] is1, Integer[] is2)
+    {
+        if (is1 == null && is2 == null)
+            return true;
+        if (is1 == null)
+            return false;
+        if (is2 == null)
+            return false;
 
         return Arrays.equals(is1, is2);
     }
 
-    public static boolean floatEquality(Float i1, Float i2) {
-        if (i1 == null && i2 == null) return true;
-        if (i1 == null) return false;
-        if (i2 == null) return false;
-
-        return i1.floatValue() == i2.floatValue();
-    }
-
-//    public static boolean boundsEquality(Object o1, Object o2) {
-//        if (o1 == o2) return true;
-//        if (o1 == null) return false;
-//        if (o2 == null) return false;
-//
-//        LatLngBounds bounds1 = (LatLngBounds) o1;
-//        LatLngBounds bounds2 = (LatLngBounds) o2;
-//
-//        if (bounds1.southwest.latitude != bounds2.southwest.latitude)
-//            return false;
-//        if (bounds1.southwest.longitude != bounds2.southwest.longitude)
-//            return false;
-//        if (bounds1.northeast.latitude != bounds2.northeast.latitude)
-//            return false;
-//        if (bounds1.northeast.longitude != bounds2.northeast.longitude)
-//            return false;
-//        return true;
-//
-//    }
-
-    public static boolean locationEquality(Location first, Location second)
+    /**
+     * Compares two locations for equality
+     *
+     * @param loc1
+     * @param loc2
+     *
+     * @return true in case of locations equality, false otherwise
+     */
+    public static boolean locationEquality(Location loc1, Location loc2)
     {
-        if (first == second) return true;
-        if (first == null) return false;
-        if (second == null) return false;
+        if (loc1 == loc2)
+            return true;
+        if (loc1 == null)
+            return false;
+        if (loc2 == null)
+            return false;
 
-        return first.getLatitude() == second.getLatitude() && first.getLongitude() == second.getLongitude();
+        return loc1.getLatitude() == loc2.getLatitude() && loc1.getLongitude() == loc2.getLongitude();
     }
 
-    public static boolean currLocaleEquality(String s1, String s2)
-    {
-        if (s1 == null && s2 == null) return true;
-        if (s1 == null) return false;
-        if (s2 == null) return false;
-
-        return s1.equals(s2);
-    }
 }
 
